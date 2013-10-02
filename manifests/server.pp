eedad0c29d01aff69da3c049db95bb62007cea2c
Joshua Hoblitt <jhoblitt@cpan.org>
2013-10-02 16:06:20 -0700

    whitespace

--- manifests/server.pp
+++ manifests/server.pp
@@ -14,8 +14,8 @@
 #
 #
 class selenium::server(
-  $display      = $selenium::params::display,
-  $options      = $selenium::params::server_options,
+  $display = $selenium::params::display,
+  $options = $selenium::params::server_options,
 ) inherits selenium::params {
   validate_string($display)
   validate_string($options)
