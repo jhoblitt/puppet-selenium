a4d77e7b8e1416544746546bc12ab2c898dea5fb
Joshua Hoblitt <jhoblitt@cpan.org>
2013-10-02 16:05:37 -0700

    add class selenium::node

---
+++ manifests/node.pp
@@ -0,0 +1,40 @@
+# == Class: selenium::node
+#
+# simple template
+#
+#
+# === Examples
+#
+# include selenium::node
+#
+#
+# === Authors
+#
+# Joshua Hoblitt <jhoblitt@cpan.org>
+#
+#
+class selenium::node(
+  $display = $selenium::params::display,
+  $options = $selenium::params::node_options,
+  $hub     = $selenium::params::default_hub,
+) inherits selenium::params {
+  validate_string($display)
+  validate_string($options)
+  validate_string($hub)
+
+  include selenium
+
+  $safe_options = "${options} -hub ${hub}"
+
+  anchor { 'selenium::node::begin': }
+  Class[ 'selenium' ] ->
+  selenium::config{ 'node':
+    display      => $display,
+    user         => $selenium::user,
+    group        => $selenium::group,
+    install_root => $selenium::install_root,
+    options      => $safe_options,
+    java         => $selenium::java,
+  } ->
+  anchor { 'selenium::node::end': }
+}
